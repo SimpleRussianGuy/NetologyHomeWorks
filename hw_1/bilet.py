fa6b69b62495692f8bbd28646bbee343523d4677
SimpleRussianGuy <elthinshordeseyk5@gmail.com>
2024-09-26 21:21:54 +0500

    update hw_1

--- hw_1/bilet.py
+++ hw_1/bilet.py
@@ -1,17 +1,4 @@
-ticket = input(
-    "Ввод => "
-)
-nums = list(ticket)
-i = 0
-sum_1 = sum_2 = 0 
-for i in range(6):
-    if i <= 2:
-        for num in nums[i]:
-            sum_1 += int(num)
-    elif i >= 3 and i <=5:
-        for num in nums[i]:
-            sum_2 += int(num)
-if sum_1 == sum_2:
-    print("Счастливый билет")
-else:
-    print("Не счастливый билет")
+ticket = input("Ввод => ")
+sum_1 = sum(map(int, ticket[:3]))
+sum_2 = sum(map(int, ticket[3:]))
+print("Счастливый билет" if sum_1 == sum_2 else "Не счастливый билет")
--- hw_1/check_year.py
+++ hw_1/check_year.py
@@ -1,7 +1,2 @@
-year = int(input(
-    "Ввод => "
-))
-if year // 400 != year / 400 and year // 4 != year / 4:
-    print("Обычный год")
-else:
-    print("Високосный год")
+year = int(input("Ввод => "))
+print("Високосный год" if (year % 400 == 0 or (year % 100 != 0 and year % 4 == 0)) else "Обычный год")
