4ea590e1bd52539449dbb689a56bb8e0eb229922
SimpleRussianGuy <elthinshordeseyk5@gmail.com>
2024-09-26 21:30:15 +0500

    check_year.py

--- hw_1/check_year.py
+++ hw_1/check_year.py
@@ -1,2 +1,1 @@
-year = int(input("Ввод => "))
-print("Високосный год" if (year % 400 == 0 or (year % 100 != 0 and year % 4 == 0)) else "Обычный год")
+year = int(input("Ввод => ")); print("Високосный год" if (year % 400 == 0 or (year % 100 != 0 and year % 4 == 0)) else "Обычный год")
